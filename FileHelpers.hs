d17302ec751de93f1c42b032da11a4e4b2c55438
delcin-raj <delcinraj@gmail.com>
2023-09-05 23:18:41 +0530

    Fixed the handle released issue
    Was calling runResourceT twice, the question is why did the type checker
    failed to catch it?
    The bigger question is why type annotations are necessary?

--- FileHelpers.hs
+++ FileHelpers.hs
@@ -2,9 +2,9 @@ module FileHelpers where
 
 import Relude
 import qualified System.Directory as Dir
-import Control.Monad.Trans.Resource (ResourceT, ReleaseKey, allocate, runResourceT)
+import Control.Monad.Trans.Resource (ResourceT, ReleaseKey, allocate)
 import qualified System.IO as IO
--- import System.FilePath ((</>))
+import System.FilePath ((</>))
 
 getDataDir :: IO FilePath
 getDataDir = do
@@ -14,6 +14,6 @@ getDataDir = do
 
 
 fileResource :: FilePath -> IO.IOMode -> ResourceT IO (ReleaseKey, IO.Handle)
-fileResource path mode = runResourceT $ do
-    -- dir <- liftIO getDataDir
-    allocate (IO.openFile path mode) IO.hClose
+fileResource path mode = do
+    dir <- liftIO getDataDir
+    allocate (IO.openFile (dir </> path) mode) IO.hClose
